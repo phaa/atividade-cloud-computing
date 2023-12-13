8165f999d996205a26aea3e2783a1823d2295703
Pedro Henrique Amorim de Azevedo <pedrohamorim2011@hotmail.com>
2023-12-13 13:47:50 -0300

    Update login.php

--- login.php
+++ login.php
@@ -83,53 +83,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	unset($pdo);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-	<meta charset="UTF-8">
-	<title>Cadastro</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<style>
-		body {
-			font: 14px sans-serif;
-		}
-
-		.wrapper {
-			width: 360px;
-			padding: 20px;
-		}
-	</style>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
 </head>
-
 <body>
-	<div class="wrapper">
-		<h2>Cadastro</h2>
-		<p>Por favor, preencha este formulário para criar uma conta.</p>
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-			<div class="form-group">
-				<label>Nome do usuário</label>
-				<input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-				<span class="invalid-feedback"><?php echo $username_err; ?></span>
-			</div>
-			<div class="form-group">
-				<label>Senha</label>
-				<input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-				<span class="invalid-feedback"><?php echo $password_err; ?></span>
-			</div>
-			<div class="form-group">
-				<label>Confirme a senha</label>
-				<input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-				<span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-			</div>
-			<div class="form-group">
-				<input type="submit" class="btn btn-primary" value="Criar Conta">
-				<input type="reset" class="btn btn-secondary ml-2" value="Apagar Dados">
-			</div>
-			<p>Já tem uma conta? <a href="login.php">Entre aqui</a>.</p>
-		</form>
-	</div>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Por favor, preencha os campos para fazer o login.</p>
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Nome do usuário</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Senha</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Entrar">
+            </div>
+        </form>
+    </div>
 </body>
-
 </html>
