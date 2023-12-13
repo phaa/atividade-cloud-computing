5233e0fb54c38afe24c4ee78a116a839681afa13
Pedro Henrique Amorim de Azevedo <pedrohamorim2011@hotmail.com>
2023-12-13 14:32:26 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,17 @@
 <?php
-session_start();
-$host = "localhost";
-$usuario = "usuario";
-$senha = "password";
-
-try {
-  $conn = new PDO("mysql:host=$host;dbname=meusistema", $usuario, $senha);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Conectado com sucesso";
-} catch(PDOException $e) {
-  echo "Falha na conexão: " . $e->getMessage();
+/* Credenciais do banco de dados. Supondo que você esteja executando o MySQL
+servidor com configuração padrão (usuário 'root' sem senha) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'usuario');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'meusistema');
+ 
+/* Tentativa de conexão com o banco de dados MySQL */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Defina o modo de erro PDO para exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Não foi possível conectar." . $e->getMessage());
 }
+?>
